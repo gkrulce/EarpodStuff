4fc86d80dfef3297d3bfebd110bc8c7ddb811ff9
Galen Krulce <gkrulce@gmail.com>
2016-09-06 13:53:26 -0700

    Got FFTW imported

--- util/Frontend.cpp
+++ util/Frontend.cpp
@@ -1,12 +1,14 @@
 #include "Frontend.hpp"
 #include <assert.h>
 #include <complex>
+#include <fftw3.h>
 
 Frontend::Frontend(int sampleRate): sampleRate_(sampleRate) {}
 
 std::vector<float> Frontend::calculate(std::vector<short> samples) {
     assert(sampleRate_ == 44100);
     assert(samples.size() == sampleRate_);
+    fftw_complex *in, *out;
     return std::vector<float>();
 }
 
