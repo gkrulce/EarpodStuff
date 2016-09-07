a8002dec59280764c706fe68cda90d66b87cde7a
Galen Krulce <gkrulce@gmail.com>
2016-09-07 08:25:05 -0700

    Installed tensorflow

--- build.sh
+++ build.sh
@@ -7,11 +7,19 @@ export HOME_DIR=`pwd`
 # Build fft library
 if [ "--full" = "$1" ]
 then
+    ## Setup fftw
     cd $HOME_DIR/fftLib
     ./configure --prefix=$HOME_DIR/fftLib/local/
     make
     make install
-    cd $HOME_DIR
+
+    # Setup tensorflow
+    cd $HOME_DIR/tensorFlow
+    virtualenv --system-site-packages tensorflowEnv
+    source tensorflowEnv/bin/activate
+    export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.10.0-py2-none-any.whl
+    pip install --upgrade $TF_BINARY_URL
+    deactivate
 fi
 
 cd $HOME_DIR/util
