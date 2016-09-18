5db7af7ea94bcace1d628686a558a35903662289
Galen Krulce <gkrulce@gmail.com>
2016-09-18 00:15:25 +0000

    Changed how data input works

--- createCsvFile.sh
+++ createCsvFile.sh
@@ -2,6 +2,23 @@ cd util/
 make clean
 make
 cd ..
-util/ConversionScript MondayNight.csv data/MondayNight/VolumeUp/ data/MondayNight/VolumeDown/ data/MondayNight/Noise/
-util/ConversionScript WednesdayMorning.csv data/WednesdayMorning/VolumeUp/ data/WednesdayMorning/VolumeDown/ data/WednesdayMorning/Noise/
-cat MondayNight.csv WednesdayMorning.csv > AllData.csv
+rm AllData.csv
+rm Validation.csv
+TRAINING_SETS="MondayNight WednesdayMorning"
+VALIDATION_SETS="Android"
+for t in $TRAINING_SETS
+do
+util/ConversionScript $t.csv data/$t/VolumeUp/ data/$t/VolumeDown/ data/$t/Noise/
+done
+
+cat `for t in $TRAINING_SETS; do echo $t.csv; done` | shuf > AllData.csv
+TOTAL_LINES=`wc -l AllData.csv | cut -f1 -d ' '`
+SPLIT_POINT=$(($TOTAL_LINES/10))
+head -n $SPLIT_POINT AllData.csv > IosTestingData.csv
+tail -n +$(($SPLIT_POINT+1)) AllData.csv > IosTrainingData.csv
+
+for t in $VALIDATION_SETS
+do
+util/ConversionScript $t.csv data/$t/VolumeUp/ data/$t/VolumeDown/ data/$t/Noise/
+cat $t.csv >> Validation.csv
+done
--- tensorFlow/simple.py
+++ tensorFlow/simple.py
@@ -5,65 +5,61 @@ import csv
 from random import shuffle
 import numpy
 import sys
-
-def one_hot(nameList, name):
-    assert(nameList.count(name) == 1)
-    l = [0] * len(nameList)
-    for idx, val in enumerate(nameList):
-        if name == val:
-            l[idx] = 1
-            return l
-    assert(false)
+import random
 
 # Should make a python dictionary here, shuffle it, and take the first X number
 # Input: csv filename
 # Output: Data structure that can be used in other functions
 def data_read(fileName):
-    
+    data = {}
     with open(fileName) as f:
-        arr = []
-        data = {}
+        data["VolumeUp"] = []
+        data["VolumeDown"] = []
+        data["Noise"] = []
         arrSize = None
         for line in f:
             csv = [val.rstrip("\n") for val in line.split(",")]
             assert(arrSize == None or arrSize == len(csv))
             arrSize = len(csv)
-            arr.append({"class":csv[0], "features":csv[1:]})
+            if csv[0] == "VolUp":
+                data["VolumeUp"].append(csv[1:])
+            elif csv[0] == "VolDown":
+                data["VolumeDown"].append(csv[1:])
+            elif csv[0] == "Noise":
+                data["Noise"].append(csv[1:])
+            else:
+                assert(false)
         
-        outputClasses = ["VolUp", "VolDown", "Noise"]
-        print "Output classes: {0}".format(outputClasses)
-        for idx, val in enumerate(arr):
-            arr[idx]["class"] = one_hot(outputClasses, arr[idx]["class"])
-
-        data["FeaturesDim"] = len(arr[0]["features"])
-        data["ClassesDim"] = len(outputClasses)
-
-        shuffle(arr)
-        dataSetSize = len(arr)
-        cutPoint = int(dataSetSize * .9) # 10% test, 90% training
-        data["TrainingData"] = arr[:cutPoint]
-        data["TestData"] = arr[cutPoint:]
-
+        data["FeaturesDim"] = arrSize-1
+        data["ClassesDim"] = 3
+        print "Read {0}".format(fileName)
         print "Number of output classes {0}".format(data["ClassesDim"])
         print "Number of features {0}".format(data["FeaturesDim"])
-        print "Traning data size: {0}".format(len(data["TrainingData"]))
-        print "Test data size: {0}".format(len(data["TestData"]))
-        
+        print "Data size (VolUp, VolDown, Noise): ({0},{1},{2})".format(len(data["VolumeUp"]), len(data["VolumeDown"]), len(data["Noise"]))
     return data
 
 # Input: data and number of samples
 # Output: tuple of random number of samples (input values, 1-hot output values)
 def data_next(data, n): 
-    shuffle(data["TrainingData"])
-    toTrain = data["TrainingData"][:n]
-    return ([val["features"] for val in toTrain], [val["class"] for val in toTrain])
+    volUp = random.sample(data["VolumeUp"], n)
+    volDown = random.sample(data["VolumeDown"], n)
+    noise  = random.sample(data["Noise"], n)
+    assert(len(volUp) == len(volDown) == len(noise))
+    l = len(volUp)
+    toReturn = volUp + volDown + noise, [[1,0,0]]*l + [[0,1,0]]*l + [[0,0,1]]*l
+    return toReturn
 
-# Input: data
-# Output: testing data tuple (input values, 1-hot output values)
-def data_test(data):
-    toTest = data["TestData"]
-    return ([val["features"] for val in toTest], [val["class"] for val in toTest])
+def data_all(data):
+    volUp = data["VolumeUp"]
+    volDown = data["VolumeDown"]
+    noise  = data["Noise"]
+    toReturn = volUp + volDown + noise, [[1,0,0]]*len(volUp) + [[0,1,0]]*len(volDown) + [[0,0,1]]*len(noise)
 
+    return toReturn
+
+# name: Name of the matrix
+# npArr: The numpy array of this matrix
+# The file name we are APPENDING to
 def dumpToFile(name, npArr, fileName):
     npShape = numpy.shape(npArr)
     rows = numpy.shape(npArr)[0]
@@ -78,47 +74,48 @@ def dumpToFile(name, npArr, fileName):
         oF.write("}; \n");
 
 def main(argv):
-    assert(len(argv) == 2)
-    inputCSVFile = argv[0]
-    outputModelFile = argv[1]
-    print("Reading CSV file located at {0} and writing a model file located at {1}".format(inputCSVFile,
-    outputModelFile))
-# Import data
-    data = data_read(inputCSVFile)
+    assert(len(argv) == 1)
+    checkpointDir = argv[0]
+    iosTrainingData = "IosTrainingData.csv"
+    iosTestingData = "IosTestingData.csv"
+    #androidTrainingData = ""
+    #androidTestingData = ""
 
-    inputDim = data["FeaturesDim"]
-    outputDim = data["ClassesDim"]
+    # Import data
+    print("Reading IOS training data from {0}, testing data from {1} and writing a checkout files to {2}".format(iosTrainingData, iosTestingData, checkpointDir))
+    training_data = data_read(iosTrainingData)
+    testing_data = data_read(iosTestingData)
 
-# Model
+    inputDim = training_data["FeaturesDim"]
+    outputDim = training_data["ClassesDim"]
+
+    # Model
     x = tf.placeholder(tf.float32, [None, inputDim])
     W = tf.Variable(tf.zeros([inputDim, outputDim]))
     b = tf.Variable(tf.zeros([outputDim]))
     y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-# Loss function
+    # Loss function
     y_ = tf.placeholder(tf.float32, [None, outputDim])
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-# Traning
+    # Traning
     train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(cross_entropy)
     init = tf.initialize_all_variables()
+    saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(init)
-        for i in range(2500):
-            batch_xs, batch_ys = data_next(data,200)
+        for i in range(5000):
+            batch_xs, batch_ys = data_next(training_data,200)
             sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-            if i % 50 == 0:
+            if i % 500 == 0:
+                saver.save(sess, checkpointDir + "/model-{0}.ckpt".format(i))
                 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
                 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-                test_xs, test_ys = data_test(data)
-                print("RESULT: {0}".format(sess.run(accuracy, feed_dict={x: test_xs, y_: test_ys})))
-                sys.stdout.flush()
-
-        with open(outputModelFile, "w") as f:
-            f.truncate()
-        dumpToFile("W", W.eval(), outputModelFile)
-        dumpToFile("B", [b.eval()], outputModelFile)
+                test_xs, test_ys = data_all(testing_data)
+                testing_accuracy = sess.run(accuracy, feed_dict={x: test_xs, y_: test_ys})
+                print("{0}\tACCURACY\t{1}".format(i, testing_accuracy))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
